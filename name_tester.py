e3445ecb7730ce52187e9eb2cfcb0683d75e8818
magnumsung <magnumsung.au@gmail.com>
2025-08-17 00:37:29 +1000

    Testing

--- name_tester.py
+++ name_tester.py
@@ -1,6 +1,9 @@
 #Procedure: main
+print('=====')
+i = 0
 u_name = input('Please enter your name: ')
-if u_name == 'Magnum':
-    print('Awesome name!')
-else:
-    print('Silly name!')
+print(f'{u_name} is a')
+while i < 10:
+    print('silly')
+    i += 1
+print('name')
